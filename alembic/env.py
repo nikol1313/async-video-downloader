a3e28ba124cae25f925b607e25a75fb6a2dcca96
angeldust <yverchita@gmail.com>
2026-07-09 19:11:59 +0400

    Updated

---
+++ alembic/env.py
@@ -0,0 +1,51 @@
+import asyncio
+from logging.config import fileConfig
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from alembic import context
+from app.db_tables import Base
+# 1. This handles the config file logging
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# 2. IMPORT YOUR MODELS' METADATA HERE SO ALEMBIC SEES YOUR TABLES
+target_metadata = Base.metadata
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode (using async engine)."""
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    # 3. Use asyncio to run the online migration method
+    asyncio.run(run_migrations_online())

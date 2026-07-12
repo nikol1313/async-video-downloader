201bb886dcbed5b75ebcfabb5fc4f62fdd9423d2
angeldust <yverchita@gmail.com>
2026-07-12 18:33:18 +0400

    ...

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,17 +6,17 @@ from fastapi.responses import FileResponse
 from httpx import ASGITransport, AsyncClient
 
 STATIC_DIR = Path(__file__).resolve().parent.parent / "app" / "static"
-test_app = FastAPI()
+app = FastAPI()
 
 
-@test_app.get("/", include_in_schema=False)
+@app.get("/", include_in_schema=False)
 async def index():
     return FileResponse(str(STATIC_DIR / "index.html"))
 
 
 @pytest.mark.asyncio
 async def test_read_root():
-    transport = ASGITransport(app=test_app)
+    transport = ASGITransport(app=app)
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
